f3f32910c0539ad5bd8885ce889ce949ecdf41aa
[Tiago] <[tiago.carv16@gmail.com]>
2023-09-18 13:53:33 -0300

    deleting multiplos.py and sort.py

---
+++ Python Projects/questao5.py
@@ -0,0 +1,16 @@
+
+n = int(input('Digite o tamanho da matriz:'))
+
+valores = []
+
+for i in range(0,n+1):
+    valor = int(input('Digite um número inteiro:'))
+    valores.append(valor)
+print(valores)
+"""
+m = int(input('Digite um número inteiro:'))
+
+for i in range(0,n):
+    if (valores[i]<m):
+        valores.remove(valores[i])
+"""
---
+++ Python Projects/sort.py
@@ -0,0 +1,4 @@
+list = [1,2,3,4,5,1,2,4,5,5,58,6]
+list = sorted(list)
+print(list)
+print(list[0])
