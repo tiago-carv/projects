744c005ed3716ee8f9a09e55f1dd5411220988e5
[Tiago] <[tiago.carv16@gmail.com]>
2024-04-09 16:11:00 -0300

    cria arquivo compilado do Exercicio1 mudanças no arquivo Exercicio1.c cria arquivo Exercicio2.c

---
+++ C projects/Listas de Intro-Prog/Lista-2/Exercicio1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void main(){
+    int horas,minutos,seg,seg_final;
+    int aux1,aux2,aux3,aux4,aux5;
+
+    scanf("%d",&horas);
+    scanf("%d",&minutos);
+    scanf("%d",&seg);
+    scanf("%d",&seg_final);
+
+aux1 = horas*3600;
+aux2 = minutos*60;
+int segundos_total = aux1+aux2+seg+seg_final;
+
+
+int seg_novo = segundos_total%60;
+int minutos_novo = (segundos_total-seg_novo)/60;
+int horas_novo = (minutos_novo-minutos_novo%60)/60;
+
+if(minutos_novo<10){
+    if(seg_novo<10){
+        printf("%d:0%d:0%d\n",horas_novo%60,minutos_novo%60,seg_novo);
+    }else{
+        printf("%d:0%d:%d\n",horas_novo%60,minutos_novo%60,seg_novo);
+    }
+}else{
+    if (seg_novo<10){
+        printf("%d:%d:0%d\n",horas_novo%60,minutos_novo%60,seg_novo);
+    }
+    else{
+        printf("%d:%d:0%d\n",horas_novo%60,minutos_novo%60,seg_novo);
+    }
+}
+}
